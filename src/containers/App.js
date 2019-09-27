0a5d79ef93d8aeb1084c5f57b4c4c520b5b86565
priyanka <begwani.priyanka5@gmail.com>
2019-09-27 19:02:21 +0530

    update app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    fetch("http://127.0.0.1:3001/contacts")
+    fetch("https://react-proxy.herokuapp.com/contacts")
       .then(res => res.json())
       .then(
         (result) => {
