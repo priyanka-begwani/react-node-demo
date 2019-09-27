ef77c1d294d14378c4c7cb8d070db228c8e934fb
priyanka <begwani.priyanka5@gmail.com>
2019-09-27 16:57:01 +0530

    initial-react-code

---
+++ src/components/contacts.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Contacts extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+   let contacts = this.props.contacts;
+   return (
+     <div>
+
+     </div>
+   )
+}
+}
+
+export default Contacts;
---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Contacts from '../components/contacts.js';
+import '../assets/App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      contacts: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:3001/contacts")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            contacts: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, contacts } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <Contacts contacts={this.state.contacts}></Contacts>
+        </div>
+      );
+    }
+  }
+}
+export default App;
